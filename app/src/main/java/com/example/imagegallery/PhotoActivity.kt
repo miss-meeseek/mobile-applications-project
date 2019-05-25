e0efeab4af53261f68ace14d0961598b2a437eec
katarzyna <236524@student.pwr.edu.pl>
2019-05-25 15:56:04 +0200

    pageView

--- app/src/main/java/com/example/imagegallery/PhotoActivity.kt
+++ app/src/main/java/com/example/imagegallery/PhotoActivity.kt
@@ -5,15 +5,23 @@ import android.graphics.Bitmap
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.StrictMode
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_photo.*
-import kotlinx.android.synthetic.main.image_detail.*
+import kotlinx.android.synthetic.main.fragment_layout.*
 import java.io.File
 
+
+
 class PhotoActivity : AppCompatActivity() {
+
+    var TAKE_PHOTO_REQUEST_CODE = 2
     var path = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
@@ -27,9 +35,27 @@ class PhotoActivity : AppCompatActivity() {
         Log.d("IMGd PHOTO (onSend)", edit_desc.text.toString())
         Log.d("Path: ", path)
 
-        val _intent = Intent(this, GalleryActivity::class.java)
+
+
+        // add photo
+        if (path != "") {
+            AsyncTask.execute {
+                GalleryDatabase.getInstance(this).userDao().insertAll(Image(0, File(path).name))
+            }
+            (supportFragmentManager.findFragmentById(R.id.titles))
+        }
+        //(supportFragmentManager!!.findFragmentById(R.id.titles) as ListFragment).notifyDataInserted()
+
+        val builder = StrictMode.VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
+
+
+
+
+        // back to MainActivity
+        val _intent = Intent(this, MainActivity::class.java)
         _intent.putExtra("path", com.example.imagegallery.path)
-        startActivityForResult(_intent, 2)
+        startActivityForResult(_intent, TAKE_PHOTO_REQUEST_CODE)
     }
 
     override fun onResume() {
